46e4e1dfadf8fd70277c7470971f2c01b4851032
Troy Chryssos <github email address>
2016-08-05 14:28:41 -0400

    Wrote methods for the student class which interact with an SQL database Wrote table creation, table deletion, instance saving, and new student creation methods for the student class which save and retrieve data with a students sql datatable.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,44 @@
+require 'pry'
+
 class Student
 
+  attr_accessor :name, :grade
+
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
+  def initialize(name, grade)
+    @name = name
+    @grade=grade
+  end
+
+  def self.create_table
+    sql="CREATE TABLE students (id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER);"
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql="DROP TABLE students;"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students(name, grade)
+      VALUES (?,?);
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+
+  def id
+    DB[:conn].execute("SELECT students.id FROM students ORDER BY students.id DESC LIMIT 1")[0][0]
+  end
+
+  def self.create(attribute_hash)
+    new_student=Student.new(attribute_hash[:name], attribute_hash[:grade])
+    new_student.save
+    new_student
+  end
 end
